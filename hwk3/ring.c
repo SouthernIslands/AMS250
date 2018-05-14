8af13a568361f69cd0104158ca7f51f8ae6f2312
SouthernIslands <renfengsui@gmail.com>
2018-05-14 11:19:36 -0700

    hwk3

---
+++ hwk3/ring.c
@@ -0,0 +1,59 @@
+#include "mpi.h"
+#include <stdio.h>
+
+int main(int argc,char *argv[]){
+    int message;
+    int size, rank, prev, next, tag;
+    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    tag = 43;
+    next = (rank + 1) % size;
+    prev = (rank + size - 1) % size;
+
+
+    if (rank == 0) {
+        message = 16;
+        
+        printf("Master process (Rank 0) initating message %d send to %d of %d process ring\n", 
+        message, next, size);
+        MPI_Send(&message, 1, MPI_INT, next, tag, MPI_COMM_WORLD); 
+    }
+
+
+    while (1) {
+        MPI_Recv(&message, 1, MPI_INT, prev, tag, MPI_COMM_WORLD, 
+            MPI_STATUS_IGNORE);
+        printf("Process %d received message %d from process %d\n",
+        rank, message, prev);
+
+        if (rank == 0){
+            message--;
+            printf("Message decrese to %d at master process\n", message);
+        }
+        
+
+        printf("Process %d sended message %d to process %d\n",
+        rank, message, next);
+        MPI_Send(&message, 1, MPI_INT, next, tag, MPI_COMM_WORLD);
+
+        if (message == 0) {
+            printf("Exiting the loop at process %d\n", rank);
+            break;
+    }
+  }
+        
+  /*  Careful! Final receive before program ends */
+
+    if (rank == 0) {
+            MPI_Recv(&message, 1, MPI_INT, prev, tag, MPI_COMM_WORLD, 
+                MPI_STATUS_IGNORE);
+            printf("Final receive before program ends\n");
+    }
+
+    MPI_Finalize();
+    return 0;
+
+}
