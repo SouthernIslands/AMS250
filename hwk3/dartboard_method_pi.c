371f549cc825b442cb8da34713c6554a4b554d36
SouthernIslands <renfengsui@gmail.com>
2018-05-15 22:48:16 -0700

    modifying

--- hwk3/dartboard_method_pi.c
+++ hwk3/dartboard_method_pi.c
@@ -1,44 +1,36 @@
 #include <stdlib.h>
 #include <time.h>
 #include "mpi.h"
-
-#define N 1E8
-#define d 1E-8
+#define N 10000000
 
 int main (int argc, char* argv[]){
-    int rank, size, result=0, sum=0;
-    double start, end, x, y;
-    
-    
+    int rank, size, sum=0, total=0;
+    double start, end, x, y, pi=0;
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    
-    MPI_Barrier(MPI_COMM_WORLD);
-    start = MPI_Wtime();
-    
+
     srand((int)time(0));
-    
-    for (i=rank; i<N; i+=size){
+
+    for (i = rank ; i < N ; i += size){
         x=rand()/(RAND_MAX+1.0);
         y=rand()/(RAND_MAX+1.0);
-        if(x*x+y*y<1.0)
-            result++;
+
+        if(x*x+y*y<1.0){
+           sum++;
+        }
     }
-    
+
     //Sum up all results
-    MPI_Reduce(&result, &sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    
-    //Synchronize all processes and get the end time
-    MPI_Barrier(MPI_COMM_WORLD);
-    end = MPI_Wtime();
-    
+    MPI_Reduce(&sum, &total, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
     //Caculate and print PI
     if (rank==0){
-        pi=4*d*sum;
-        printf("np=%2d;    Time=%fs;    PI=%0.4f\n", size, end-begin, pi);
+        pi = (double) 4*total/N;
+        printf("np=%2d   PI=%0.5f\n", size, pi);
     }
-    
+
     MPI_Finalize();
     return 0;
 }
