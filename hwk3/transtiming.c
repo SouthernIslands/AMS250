d949ee9b0a2155414d295b605d9b01dcf6447b5e
SouthernIslands <renfengsui@gmail.com>
2018-05-15 19:58:08 -0700

    testing

--- hwk3/transtiming.c
+++ hwk3/transtiming.c
@@ -1,14 +1,14 @@
 #include "mpi.h"
 #include <stdio.h>
 #define N 1000
-#define L 1000
+#define L 1000000
 
 int main(int argc,char *argv[]){
-    int i;
+    int i, len = 1;
     int size, rank, tag=1;
     double start, end, time;
-    int *msgsend = (int *) malloc(10000*sizeof(int));
-    int *msgrecv = (int *) malloc(10000*sizeof(int));
+    int *msgsend = (int *) malloc(L*sizeof(int));
+    int *msgrecv = (int *) malloc(L*sizeof(int));
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -17,22 +17,31 @@ int main(int argc,char *argv[]){
 
     MPI_Barrier(MPI_COMM_WORLD);
     if(rank == 0){
+        while(len <= L){
+          start = MPI_Wtime();
+          for(i = 1; i <= N ; i++){
+            MPI_Send(&msgsend[0], len, MPI_INT, 1, tag, MPI_COMM_WORLD);
+            MPI_Recv(&msgrecv[0], len, MPI_INT, 1, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+          }
+          end = MPI_Wtime();
+          time = end - start;
 
-      start = MPI_Wtime();
-      for(i = 1; i <= N ; i++){
-        MPI_Send(&msgsend[0], L, MPI_INT, 1, tag, MPI_COMM_WORLD);
-        MPI_Recv(&msgrecv[0], L, MPI_INT, 1, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      }
-      end = MPI_Wtime();
-      time = end - start;
-
-      printf("Average time is %f seconds\n", time/100);
+          printf("Average time is %f seconds\n", time/N);
+          printf("While message length is %d \n", len);  
+        } 
+      len = len*100;  
+      }  
     }
     if(rank == 1){
+      while(len <= N){
       for(i = 1; i <= N ; i++){
-        MPI_Recv(&msgrecv[0], L, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Send(&msgsend[0], L, MPI_INT, 0, tag, MPI_COMM_WORLD);
+        MPI_Recv(&msgrecv[0], len, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Send(&msgsend[0], len, MPI_INT, 0, tag, MPI_COMM_WORLD);
+        }
+        len = len*100;
       }
     }
-
+    
+    MPI_Finalize();
+    return 0;
 }
