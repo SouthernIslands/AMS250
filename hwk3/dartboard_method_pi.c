4a772e44829e30827b473f4124fc63624446bf26
SouthernIslands <renfengsui@gmail.com>
2018-05-15 22:58:56 -0700

    古巴比伦王颁布了汉谟拉比法典

--- hwk3/dartboard_method_pi.c
+++ hwk3/dartboard_method_pi.c
@@ -4,16 +4,18 @@
 #define N 10000000
 
 int main (int argc, char* argv[]){
-    int rank, size, sum=0, total=0;
+    int i, sum = 0, total = 0;
+    long rank, size, average;
     double start, end, x, y, pi=0;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    srand((int)time(0));
+    average = N/size;
+    srand((int)time(123));
 
-    for (i = rank ; i < N ; i += size){
+    for (i = rank ; i < average ; i++){
         x=rand()/(RAND_MAX+1.0);
         y=rand()/(RAND_MAX+1.0);
 
@@ -26,7 +28,7 @@ int main (int argc, char* argv[]){
     MPI_Reduce(&sum, &total, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
     //Caculate and print PI
-    if (rank==0){
+    if (rank == 0){
         pi = (double) 4*total/N;
         printf("np=%2d   PI=%0.5f\n", size, pi);
     }
