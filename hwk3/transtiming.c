2be153930eaf33ee5aec7349927b895a447c1afd
SouthernIslands <renfengsui@gmail.com>
2018-05-15 18:03:23 -0700

    first submission

---
+++ hwk3/transtiming.c
@@ -0,0 +1,37 @@
+#include <time.h>
+#include "mpi.h"
+#include <stdio.h>
+
+int main(int argc,char *argv[]){
+    int n;
+    int size, rank, tag=1;
+    double start, end, time;
+    int *msgsend = (int *) malloc(10000*sizeof(int));
+    int *msgrecv = (int *) malloc(10000*sizeof(int));
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    if(rank == 0){
+
+      start = MPI_Wtime();
+      for(n = 1; n <= 100 ; i++){
+        MPI_Send(&msgsend[0], 1, MPI_INT, 1, tag, MPI_COMM_WORLD);
+        MPI_Recv(&msgrecv[0], 1, MPI_INT, 1, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      }
+      end = MPI_Wtime();
+      time = end - start;
+
+      printf("Average time is %f seconds\n", time/100);
+    }
+    if(rank == 1){
+      for(n = 1; n <= 100 ; i++){
+        MPI_Recv(&msgrecv[0], 1, MPI_INT, 1, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Send(&msgsend[0], 1, MPI_INT, 1, tag, MPI_COMM_WORLD);
+      }
+    }
+
+}
