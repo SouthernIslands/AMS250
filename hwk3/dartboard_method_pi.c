8b48e6f51a26bbc34892570585b7622cef8aa7b9
SouthernIslands <renfengsui@gmail.com>
2018-05-15 23:26:04 -0700

    一盏离愁孤灯伫立在窗口

--- hwk3/dartboard_method_pi.c
+++ hwk3/dartboard_method_pi.c
@@ -1,36 +1,34 @@
 #include <stdlib.h>
 #include <time.h>
 #include "mpi.h"
-#define N 10000000
+#define N 1000000000
 
 int main (int argc, char* argv[]){
-    int i, sum = 0, total = 0;
-    long rank, size, average;
-    double start, end, x, y, pi=0;
+    long average, i, sum = 0, total = 0;
+    int rank, size;
+    double start, end, x, y, pi;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    average = N/size;
-    srand((int)time(123));
+    srand(time(NULL));
+    average = ((long)N/size);
 
     for (i = rank ; i < average ; i++){
-        x=rand()/(RAND_MAX+1.0);
-        y=rand()/(RAND_MAX+1.0);
+        x=((double)rand()/RAND_MAX);
+        y=((double)rand()/RAND_MAX);
 
         if(x*x+y*y<1.0){
-           sum++;
+            sum++;
         }
     }
 
-    //Sum up all results
     MPI_Reduce(&sum, &total, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    //Caculate and print PI
     if (rank == 0){
-        pi = (double) 4*total/N;
-        printf("np=%2d   PI=%0.5f\n", size, pi);
+        pi = ((double) 4*total/N );
+        printf("PI is %0.5f computing on %d processes \n", pi, size);
     }
 
     MPI_Finalize();
